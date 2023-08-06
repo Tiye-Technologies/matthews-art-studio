fd9431f5844dc1a635a0f3b047bc7b20b283e0d7
ThibaMahlezana <thiba.ma@gmail.com>
2023-08-06 03:40:10 +0200

    website upgrade

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function($){
+    
+    // Initialize Glightbox
+    const glightbox = GLightbox({
+        selector: '.glightbox'
+    });
+
+    // Initialize AOS
+    AOS.init();
+
+})(jQuery);
