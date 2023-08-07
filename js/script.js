ead85489297c219199714ae4d5e97bb1f385157c
ThibaMahlezana <thiba.ma@gmail.com>
2023-08-07 18:39:16 +0200

    completed website

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 (function($){
-    
     // Initialize Glightbox
     const glightbox = GLightbox({
         selector: '.glightbox'
@@ -7,5 +6,46 @@
 
     // Initialize AOS
     AOS.init();
+
+    // Back to top button
+    $('.back-to-top').click(function(){
+        $("html, body").animate({ scrollTop: "0" });
+        return false;
+    });
+
+    // Preloader
+    let preloader =  $('#preloader');
+    if (preloader) {
+        window.addEventListener('load', () => {
+        preloader.remove()
+        });
+    }
+    // Navigation hamburger
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menu");
+
+    hamburger.addEventListener("click", ()=> {
+        hamburger.classList.toggle("active");
+        navMenu.classList.toggle("active");
+    })
+
+    document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", ()=> {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
+
+    // Header on scroll
+    const header = document.querySelector("#header");
+    if(header) {
+        window.onscroll = () => {
+            if(scrollY > 100){
+                header.classList.add("active");
+            }
+            else{
+                header.classList.remove("active");
+            }
+        }
+    }
+    
 
 })(jQuery);
